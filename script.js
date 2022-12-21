f3e3f8ac1546a19b3d715ce888ca7e705a5dfa04
dipak307 <114248721+dipak307@users.noreply.github.com>
2022-12-21 14:06:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+let input=document.getElementById("input");
+let expression= "";
+function solve(e){
+   // console.log(e)
+   if(e =='ans'){
+ let result=eval(expression);
+ input.value=result;
+   }
+   else if(e == "clear"){
+    expression="";
+    input.value="";
+   }
+   else{
+    expression+=e;
+    input.value=expression;
+   }
+}
